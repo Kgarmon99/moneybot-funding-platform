1c97853f20fce3d89aaab8c658d07a43a58d62f3
Kahlil <kahlil@getmoneybot.com>
2026-06-05 11:16:18 -0500

    Rebuild for deployment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-const BASENAME = '/moneybot-funding-platform';
+const BASENAME = '/';
 import App from './App';
 import './index.css';
 
